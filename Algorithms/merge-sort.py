8bac30150df8d2f6dc870cd542999df3b16e883a
JesperBry <jesper_bry@hotmail.com>
2017-09-04 20:22:49 +0200

    Add files via upload

---
+++ Algorithms/merge-sort.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+# MERGE-Sort
+# mergeSort is merge sort function.
+# A is array, p is start point and r is end point.
+def mergeSort(A, p, r):
+    if p < r:
+        q = (p + r)//2
+        mergeSort(A, p, q)
+        mergeSort(A, q + 1, r)
+        merge(A, p, q, r)
+
+# merge
+    # A = array/list
+    # p,q,r: value of array ,p<=q<r
+def merge(A, p, q, r):
+    n1 = q - p + 1
+    n2 = r - q
+    L = [0]*(n1 + 1)
+    R = [0]*(n2 + 1)
+    for i in range(0, n1):
+        L[i] = A[p + i]
+    for j in range(0, n2):
+        R[j] = A[q + j + 1]
+    L[n1] = sys.maxsize # maximum value a variable of type Py_ssize_t can take
+    R[n2] = sys.maxsize
+    i = 0
+    j = 0
+    for k in range(p, r + 1):
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+
+
+A=[2,6,16,8,7,6,4,5,7,12]
+
+print('before sort', A)
+mergeSort(A, 0, len(A)-1)
+print('after sort', A)
