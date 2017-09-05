334554ff3eb14a63bf1ef0b19866278a5b610f39
JesperBry <jesper_bry@hotmail.com>
2017-09-05 12:39:13 +0200

    Update Insertion-sort(A).py

--- Algorithms/Insertion-sort(A).py
+++ Algorithms/Insertion-sort(A).py
@@ -2,13 +2,13 @@
 
 # Insertion-sort
 def insertionSort(A):
-    for j in range(0, len(A)):
+    for j in range(1, len(A)):
         key = A[j]
         i = j-1
-        while i > 0 and A[i] > key:
+        while i > -1 and A[i] > key:
             A[i+1] = A[i]
             i = i-1
         A[i+1] = key
     return A
 
-print(insertionSort([2,6,7,8,13,2,7,9,3]))
+print(insertionSort([18,2,6,7,8,13,1,7,9,3]))
